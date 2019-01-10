77a16374ac02801804b26b6ddd03351254a38d36
Jun Ito <jun_ito@voyagegroup.com>
2019-01-10 18:28:45 +0900

    Update: テストが一通り動きそうなことを確認

--- scripts/main.py
+++ scripts/main.py
@@ -1,13 +1,21 @@
-from typing import Optional
+from typing import *
 
 
-def main():
-    message = get_message()
-    print(message)
+def main() -> None:
+    r = 2
+    c = Circle(r)
+    area = c.area()
+    print(area)
 
 
-def get_message() -> str:
-    return 'message'
+class Circle:
+
+    def __init__(self, radius: float) -> None:
+        self.radius = radius
+        self.pi = 3.14
+
+    def area(self) -> float:
+        return self.radius**2 * self.pi
 
 
 if __name__ == '__main__':
