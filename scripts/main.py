abc741a6365b951fedad32e1eac19fbe4f12b8c2
Jun Ito <jun_ito@voyagegroup.com>
2019-01-10 18:33:48 +0900

    Update: 独自クラスも型ヒントできることを確認

--- scripts/main.py
+++ scripts/main.py
@@ -4,8 +4,12 @@ from typing import *
 def main() -> None:
     r = 2
     c = Circle(r)
+
     area = c.area()
+    origin = c.origin()
+
     print(area)
+    print(origin)
 
 
 class Circle:
@@ -16,6 +20,19 @@ class Circle:
 
     def area(self) -> float:
         return self.radius**2 * self.pi
+
+    def origin(self) -> 'Point':
+        return Point(0, 0)
+
+
+class Point:
+
+    def __init__(self, x: float, y: float) -> None:
+        self.x = x
+        self.y = y
+
+    def __str__(self) -> str:
+        return 'Point ({}, {})'.format(self.x, self.y)
 
 
 if __name__ == '__main__':
