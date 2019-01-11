3465c882978c19858ee2b934839c24b3da785d16
Jun Ito <jun_ito@voyagegroup.com>
2019-01-11 17:52:54 +0900

    Add: test case

--- tests/modules/test_point.py
+++ tests/modules/test_point.py
@@ -5,8 +5,10 @@ from scripts.modules.point import Point
 
 class Test_TestPoint(unittest.TestCase):
     def test_distance(self):
-        p = Point(3, 4)
-        self.assertEqual(p.distance_from_origin(), 5)
+        cases = [(Point(0, 0), 0), (Point(3, 4), 5), (Point(-3, -4), 5)]
+
+        for p, expected in cases:
+            self.assertEqual(p.distance_from_origin(), expected)
 
 
 if __name__ == "__main__":
