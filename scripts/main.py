b662c5bfe11a1301bba92af07b5702de7f7628e4
Jun Ito <jun_ito@voyagegroup.com>
2019-01-11 16:19:37 +0900

    Update: vscode でautoformatがされるように

--- scripts/main.py
+++ scripts/main.py
@@ -1,6 +1,3 @@
-from typing import *
-
-
 def main() -> None:
     r = 2
     c = Circle(r)
@@ -13,27 +10,25 @@ def main() -> None:
 
 
 class Circle:
-
     def __init__(self, radius: float) -> None:
         self.radius = radius
         self.pi = 3.14
 
     def area(self) -> float:
-        return self.radius**2 * self.pi
+        return self.radius ** 2 * self.pi
 
-    def origin(self) -> 'Point':
+    def origin(self) -> "Point":
         return Point(0, 0)
 
 
 class Point:
-
     def __init__(self, x: float, y: float) -> None:
         self.x = x
         self.y = y
 
     def __str__(self) -> str:
-        return 'Point ({}, {})'.format(self.x, self.y)
+        return "Point ({}, {})".format(self.x, self.y)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
