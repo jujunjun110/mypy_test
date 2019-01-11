62b8372eb57d70c7ea89bd8262d129be7ce8a50e
Jun Ito <jun_ito@voyagegroup.com>
2019-01-11 17:20:32 +0900

    Add: 正しく機能を確認できるテストを追加

--- tests/modules/test_circle.py
+++ tests/modules/test_circle.py
@@ -1,10 +1,16 @@
 import unittest
 
+from scripts.modules.point import Point
+from scripts.modules.circle import Circle
+
 
 class Test_TestCircle(unittest.TestCase):
     def test_area(self):
-        self.assertEqual(5, 5)
-        # self.assertEqual(Circle(3, Point(0, 0)), 28.26)
+        c = Circle(0, Point(0, 0))
+        self.assertEqual(c.area(), 0)
+
+        c = Circle(3, Point(0, 0))
+        self.assertEqual(c.area(), 28.26)
 
 
 if __name__ == "__main__":
