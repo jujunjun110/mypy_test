5999048912b2aaaf498eb9a60d348be7f41b2f84
Jun Ito <jun_ito@voyagegroup.com>
2019-01-21 14:56:26 +0900

    Update: error check

--- scripts/modules/myparser.py
+++ scripts/modules/myparser.py
@@ -4,9 +4,13 @@ from typing import Union
 
 class MyParser:
     def extract_number(self, text: str) -> Union[float, None]:
-        res = re.findall("[-.\d]{1}", text)
+        try:
+            res = re.findall("[-.\d]{1}", text)
 
-        if len(res) == 0:
+            if len(res) == 0:
+                return None
+
+            return float("".join(res))
+
+        except Exception:
             return None
-
-        return float("".join(res))
